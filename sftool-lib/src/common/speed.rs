4aa90470d913b9a61400d2fb2c9c617e91c911cb
HalfSweet <haoranjiang@sifli.com>
2025-11-03 15:01:22 +0800

    perf: 确定切换完毕之后才进行

--- sftool-lib/src/common/speed.rs
+++ sftool-lib/src/common/speed.rs
@@ -1,6 +1,5 @@
-use crate::common::ram_command::{Command, RamCommand};
+use crate::common::ram_command::{Command, RamCommand, RamOps};
 use crate::{Result, SifliToolTrait};
-use std::io::Write;
 use std::time::Duration;
 
 /// 通用的速度设置操作实现
@@ -23,16 +22,10 @@ impl SpeedOps {
 
         // 等待一段时间让设置生效
         std::thread::sleep(Duration::from_millis(300));
-
-        // 发送回车换行测试连接
-        tool.port().write_all("\r\n".as_bytes())?;
-        tool.port().flush()?;
-
-        // 再等待一段时间
-        std::thread::sleep(Duration::from_millis(300));
-
-        // 清空缓冲区
+        
         tool.port().clear(serialport::ClearBuffer::All)?;
+
+        RamOps::wait_for_shell_prompt(tool.port(), b"msh >", 200, 5)?;
 
         Ok(())
     }
